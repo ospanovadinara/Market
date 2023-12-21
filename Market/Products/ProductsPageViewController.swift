99e22d5c3f71275790cbc60f37def110c3380a0e
ospanovadinara <ospan.dinara@yandex.ru>
2023-12-21 15:06:55 +0600

    Начала верстать экран с продуктами

--- Market/Products/ProductsPageViewController.swift
+++ Market/Products/ProductsPageViewController.swift
@@ -5,4 +5,55 @@
 //  Created by Dinara on 21.12.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ProductsPageViewController: UIViewController {
+
+    // MARK: - UI
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }()
+
+    private lazy var segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl()
+        return segmentedControl
+    }()
+
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }()
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationController()
+        setupViews()
+        setupConstraints()
+    }
+}
+
+private extension ProductsPageViewController {
+    func setupNavigationController() {
+        self.navigationItem.title = "Продукты"
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "left_arrow_icon"),
+            style: .plain,
+            target: self,
+            action: #selector(backButtonDidTap))
+    }
+
+    func setupViews() {
+
+    }
+
+    func setupConstraints() {
+
+    }
+
+    @objc
+    func backButtonDidTap() {
+        //TODO
+    }
+}
